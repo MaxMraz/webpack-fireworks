547d4f152121f5d5953e06b55ff9876553f4e4e9
MaxMraz <Max.Mraz.11@gmail.com>
2018-12-17 14:56:53 -0500

    setup for GH-Pages

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import Fireworks from 'fireworks-canvas';
 export default () => {
   const container = document.getElementById('container');
   const options = {
-    maxRockets: 200,            // max # of rockets to spawn
+    maxRockets: 500,            // max # of rockets to spawn
     rocketSpawnInterval: 10, // millisends to check if new rockets should spawn
     numParticles: 100,        // number of particles to spawn when rocket explodes (+0-10)
-    explosionMinHeight: 0.2,  // percentage. min height at which rockets can explode
+    explosionMinHeight: 0.4,  // percentage. min height at which rockets can explode
     explosionMaxHeight: 1,  // percentage. max height before a particle is exploded
     explosionChance: 0.04     // chance in each tick the rocket will explode
     }
